e737800e56133cb7ea7d306121cafc92fc29815b
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-24 14:37:45 +0200

    Implements INuotante interface

--- csharp-abstract-animals/Delfino.cs
+++ csharp-abstract-animals/Delfino.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace csharp_abstract_animals
 {
-    internal class Delfino : Animale
+    internal class Delfino : Animale, INuotante
     {
         public override void Mangia()
         {
             Console.WriteLine("Delifino mangia pesci");
+        }
+
+        public void Nuota()
+        {
+            Console.WriteLine("Delfino nuota");
         }
 
         public override void Verso()
